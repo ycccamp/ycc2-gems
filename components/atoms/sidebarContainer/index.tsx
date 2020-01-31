a85f5c20b7eb7619d593f7e131d99d393be461ad
SaltyAom <aomkirby@mystiar.com>
2020-01-31 07:37:12 +0700

    :tada: Add tslint

--- components/atoms/sidebarContainer/index.tsx
+++ components/atoms/sidebarContainer/index.tsx
@@ -1,16 +1,16 @@
-import { memo, FunctionComponent } from "react"
+import { memo, FunctionComponent } from 'react'
 
-import "./sidebar.styl"
+import './sidebar.styl'
 
-import SidebarContainerProps from "./types"
+import SidebarContainerProps from './types'
 
 const SidebarContainer: FunctionComponent<SidebarContainerProps> = memo(
-	({ children, bottom }) => (
-		<aside id="sidebar">
-			<section className="area">{children}</section>
-			<footer className="area">{bottom}</footer>
-		</aside>
-	)
+    ({ children, bottom }) => (
+        <aside id="sidebar">
+            <section className="area">{children}</section>
+            <footer className="area">{bottom}</footer>
+        </aside>
+    )
 )
 
 export default SidebarContainer
